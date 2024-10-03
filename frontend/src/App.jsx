39297ae42f6f527381c40b7b0bbea47f235ee52e
Kartik Pal <138274869+Kartikkumar123@users.noreply.github.com>
2024-10-03 16:27:32 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import {Routes,Route} from 'react-router-dom'
+import {Router,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
