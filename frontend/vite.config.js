5d8c474d5140d04a4a9e6b3cfbd2b5ebd6d89a05
Kartik Pal <138274869+Kartikkumar123@users.noreply.github.com>
2024-10-03 16:54:16 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{port:5173,usePolling:true},
    build: {
     rollupOptions: {
-      external: ['react-router-dom']
+      external: ['react-router-dom','react-toastify']
     }
   }
 })
