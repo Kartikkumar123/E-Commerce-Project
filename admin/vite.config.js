2360ede6c99f51ba6b3e6dd38666d4883c7ccef5
Kartik Pal <138274869+Kartikkumar123@users.noreply.github.com>
2024-10-04 12:03:31 +0530

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{port:5174},
-   optimizeDeps: {
-    include: ['react-toastify'],
+  build: {
+    rollupOptions: {
+      external: ['react-toastify'],
+    },
   },
 })
