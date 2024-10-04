d30ce6a67fe075a07e92fe1ea274c384509bde38
Kartik Pal <138274869+Kartikkumar123@users.noreply.github.com>
2024-10-04 14:46:42 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173,usePolling:true},
-    build: {
+  server: {
+    port: 5173,
+    usePolling: true,
+  },
+  build: {
+    // Do not externalize react-router-dom, react-toastify, etc.
     rollupOptions: {
-      external: ['react-router-dom','react-toastify','react-toastify/dist/ReactToastify.css','axios'],
+      external: [],
     },
   },
-})
+});
